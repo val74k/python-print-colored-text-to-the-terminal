826ded74c6c424b18408fe0b6bac135f1656df6e
val.74k <60100342+val74k@users.noreply.github.com>
2022-10-01 15:42:56 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+print("\033[90m okgrey")
+print("\033[91m okred")
+print("\033[92m okgreen")
+print("\033[93m okyellow")
+print("\033[94m okblue")
+print("\033[95m okpurple")
+print("\033[96m okcyan")
+print("\033[97m okwhite")
+
+print()
+
+print("\x1b[30m black")
+print("\x1b[31m red")
+print("\x1b[32m green")
+print("\x1b[33m yellow")
+print("\x1b[34m blue")
+print("\x1b[35m magenta")
+print("\x1b[36m cyan")
+print("\x1b[37m grey")
+print("\x1b[38m white")
+
+print()
+
+print("\033[7m background white")
+print("\033[0m")#for end background white
+print("\x1b[40m background black")
+print("\x1b[41m background red")
+print("\x1b[42m background green")
+print("\x1b[43m background yellow")
+print("\x1b[44m background blue")
+print("\x1b[45m background purple")
+print("\x1b[46m background cyan")
+print("\x1b[47m background greyred")
+print("\x1b[48m background normal")
+
+print()
+
+print("\033[1m bold")
+print("\033[0m endc")
+print("\033[3m italic")
+print("\033[0m")#for end italic
+print("\033[4m underline")
+print("\033[0m")#for end underlinei
+
+
+print("\33[91m fail")
+print("\33[93m warning")
+print("\33[94m blue")
+print("\33[95m warning")
+print("\33[96m cyan")
+
+print()
+print("\033[0m FOR RESET")
